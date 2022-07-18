b81a6f6205bbe1b7cdcfa8cadabec6f8e342a433
zeev mindali <mindalizeev@gmail.com>
2022-07-18 13:37:24 +0300

    add the div for display in page

--- targil_18_07_ajax/js/myJS.js
+++ targil_18_07_ajax/js/myJS.js
@@ -11,8 +11,58 @@ $("#searchCar").click(()=>{
 
 
 const showCarData = (data)=>{    
-    console.log(data);
     let myData = data.result.records[0]; 
+    console.log(getCarString(myData));
+
 };
 
+const getCarString =(data)=>{
+    // create the data we want
+    // let myString= `<div>${data.tozeret_nm}</div>`;
+    // myString+=`<div>${data.kinuy_mishari}</div>`;
+    // myString+=`<div>${data.shnat_yitzur}</div>`;
+    // myString+=`<div>${data.tzeva_rechev}</div>`;
+    // myString+=`<div>${data.sug_delek_nm}</div>`;
+    // myString+=`<div>${data.tokef_dt}</div>`;
+    // //return the final string
+    // return myString;
+
+    let myString = `
+    <div>${data.tozeret_nm}</div>
+    <div>${data.kinuy_mishari}</div>
+    <div>${data.shnat_yitzur}</div>
+    <div>${data.tzeva_rechev}</div>
+    <div>${data.sug_delek_nm}</div>
+    <div>${convertDate(data.tokef_dt)}</div>
+    `
+
+    return myString;
+}
+
+//new style
+const convertDate = str=>{
+    //2023-07-11T00:00:00
+    const tokef_dt = str.split("T")[0]; // => create an array which the word T will split the string and take the first place
+    //2023-07-11T00:00:00 => ["2023-07-11","00:00:00"] and we want only the first place [0]
+    //2023-07-11
+    const newDate = tokef_dt.split("-");
+    //2023-07-11 => will split by - =>["2023","07","11"]
+    return newDate[2]+"/"+newDate[1]+"/"+newDate[0];
+    //11/07/2023 
+}
+
+//old style
+function kartzya(str){
+    const tokef_dt = str.split("T")[0];
+    const newDate = tokef_dt.split("-");
+    return newDate[2]+"/"+newDate[1]+"/"+newDate[0];
+}
+/*
+    tozeret_nm
+    kinuy_mishari
+    shnat_yitzur
+    tzeva_rechev
+    sug_delek_nm
+    tokef_dt
+*/
 
