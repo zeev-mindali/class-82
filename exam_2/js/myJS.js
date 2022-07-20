a797554a10035384f894400d9d50af54c69ea22e
zeev mindali <mindalizeev@gmail.com>
2022-07-20 10:00:16 +0300

    kartzya

--- exam_2/js/myJS.js
+++ exam_2/js/myJS.js
@@ -1,13 +1,46 @@
-const allData = ()=>{
+const myURL = "https://restcountries.com/v3.1/all";
+var filterData;
+
+const getData = (filterData)=>{
     //get data from server with rest
+    $.ajax({ 
+        url: myURL,
+        success : data=>{
+            if (filterData===undefined){
+                filterData = data;
+                 console.log(data);
+            }
+            filterData = data;
+            console.log(data);
+            totalCountries(data.length);
+        }
+    })
 };
 
 $("#findAll").click(()=>{
-    createTable();
+    getData();
 })
 
+const totalCountries = (jsonTotal)=>{$("#totalC").html(`total countries: ${jsonTotal}`)};
 
 
 const createTable = ()=>{};
 
-const findFilter = ()=>{}
+const findFilter = ()=>{
+    getData($("#cName").val());
+}
+
+
+// const matan = (name,age,height)=>{
+//     if (age==undefined){
+//         age=0;
+//     }
+//     if (height==undefined){
+//         height=0;
+//     }
+//     console.log("name:",name," age:",age," height:",height);
+// }
+
+// matan("Matan");
+// matan("Matan",2);
+// matan("Matan",2,180);
