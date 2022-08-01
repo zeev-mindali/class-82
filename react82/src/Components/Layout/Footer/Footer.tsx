09ac2141024795393d909b20ce97c31778284617
zeev mindali <mindalizeev@gmail.com>
2022-08-01 14:39:28 +0300

    react

--- react82/src/Components/Layout/Footer/Footer.tsx
+++ react82/src/Components/Layout/Footer/Footer.tsx
@@ -1,9 +1,17 @@
+import { useEffect, useState } from "react";
 import "./Footer.css";
 
 function Footer(): JSX.Element {
+    const madeByArray = ["Zeev","Eden","Haron","Elior","Noa","Geni","Maya","Amit","Dolev","Daniel"];
+    let madeBy = madeByArray[0];
+    setInterval(()=>{
+        const index = Math.floor(Math.random()*madeByArray.length);
+        madeBy = madeByArray[index];
+        console.log(index);
+    },1000);
     return (
         <div className="Footer">
-			all right &reg; is reseved to zeev mother in law 666
+			all right &reg; is reseved to {madeBy}
         </div>
     );
 }
--- react82/src/Components/Layout/Header/Header.tsx
+++ react82/src/Components/Layout/Header/Header.tsx
@@ -1,9 +1,20 @@
+import { useState } from "react";
 import "./Header.css";
 
 function Header(): JSX.Element {
+    // we have a tuple (צמד) with two items
+    // first item refer to the variable name
+    // second item refer to the function for updating the variable.
+    //      var     fun          init
+    const [myTime,setHeaderTime] = useState("");
+    //let myTime = "";
+    setInterval(()=>{
+        //myTime = new Date().toLocaleTimeString();
+        setHeaderTime(new Date().toLocaleTimeString());
+    },1000);
     return (
         <div className="Header">
-			<h1>Header</h1>
+			<h1>{myTime}</h1>
         </div>
     );
 }
