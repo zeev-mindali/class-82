bf954ecefdfad541b4a9bfaac4f2e546e20be4d2
zeev mindali <mindalizeev@gmail.com>
2022-08-01 09:06:29 +0300

    test

---
+++ react_is_easy/react_is_easy/src/Components/Layout/MainLayout/MainLayout.tsx
@@ -0,0 +1,11 @@
+import "./MainLayout.css";
+
+function MainLayout(): JSX.Element {
+    return (
+        <div className="MainLayout">
+			Hello Dudi the king<hr/>
+        </div>
+    );
+}
+
+export default MainLayout;
