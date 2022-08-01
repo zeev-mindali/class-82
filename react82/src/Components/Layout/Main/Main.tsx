b8e66fcca21cd07c71813221da227854bfcd8d27
zeev mindali <mindalizeev@gmail.com>
2022-08-01 10:31:24 +0300

    react 01 08

--- react82/src/Components/Layout/Main/Main.tsx
+++ react82/src/Components/Layout/Main/Main.tsx
@@ -3,42 +3,57 @@ import "./Main.css";
 function Main(): JSX.Element {
     const tasks = [
         {
+            id: 1,
             name: "clean the house",
             isDone: true,
             owner : "zeev",
         },
         {
+            id: 2,
             name: "take out garbage",
             isDone: false,
             owner : "zeev",
         },
         {
+            id: 3,
             name : "install lamps",
             isDone: false,
             owner : "zeev",
         },
         {
+            id: 4,
             name : "make fish for friday",
             isDone : false,
             owner: "zeev",
         },
         {
+            id: 5,
             name : "wash the house",
             isDone : true,
             owner : "Amital",
         },
         {
+            id: 6,
             name : "take care for the elevetors",
             isDone: true,
             owner : "Amital",
         }
     ]
+
+    const doneTasks = tasks.filter(item=>item.isDone) // ... => spread operator
+    doneTasks[0].isDone = false;
+    tasks[0].isDone = true;
+
+    /*  Please don't !!!!
+        const doneTasks_oldStyle = [];
+        const copyArray = tasks.map(item=>doneTasks_oldStyle.push(item));
+    */
     return (
         <div className="Main">
 			<h1>Main</h1><hr/>
             <div id="kartzya">
                 {/*<iframe width="560" height="315" src="https://www.youtube.com/embed/U8qUDoBt_Dg" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>*/}
-                {tasks.map(item=><div className="Box">{item.name}<br/>{item.isDone?"done":"וואי עלייך"}<br/>{item.owner}</div>)}
+                {doneTasks.map(item=><div className="Box" key={item.id}>{item.name}<br/>{item.isDone?"done":"וואי עלייך"}<br/>{item.owner}</div>)}
             </div>
             
         </div>
