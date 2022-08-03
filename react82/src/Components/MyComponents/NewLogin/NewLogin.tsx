850a6ae1ee8fce61cf238f2f6caa850b71650c7b
zeev mindali <mindalizeev@gmail.com>
2022-08-03 14:11:49 +0300

    react material ui

--- react82/src/Components/MyComponents/NewLogin/NewLogin.tsx
+++ react82/src/Components/MyComponents/NewLogin/NewLogin.tsx
@@ -1,17 +1,21 @@
-import { AccountBox, CheckBox, Password } from "@mui/icons-material";
-import { Button, ButtonGroup, TextField, Typography } from "@mui/material";
+
+import { Typography, TextField, Button, ButtonGroup } from "@material-ui/core";
+import { AccountBox, CheckBox, ContactPhone } from "@material-ui/icons";
+
 import "./NewLogin.css";
 
 function NewLogin(): JSX.Element {
+    //npm i @material-ui/core --legacy-peer-deps  
+    //npm i @material-ui/icons --legacy-peer-deps 
     return (
         <div className="NewLogin Box">
 			<Typography variant="h4" className="HeadLine">System Login</Typography>
             <AccountBox style={{fontSize:40, margin: 10}}/>
             <TextField label="user name" variant="outlined" inputProps={{ style: { color: "white" }}}/><br/><br/>
-            <Password style={{fontSize:40, margin:10}}/>
+            <ContactPhone style={{fontSize:40, margin:10}}/>
             <TextField label="password" type="password"/><br/><br/>
             <CheckBox/>remember me<br/><br/>
-            <ButtonGroup variant="contained" fullWidth>
+            <ButtonGroup variant="contained" aria-label="outlined primary button group">
                 <Button color="primary">Send</Button>
                 <Button color="secondary">cancel</Button>
             </ButtonGroup>
