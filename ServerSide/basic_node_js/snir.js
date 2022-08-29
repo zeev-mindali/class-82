7428129a09937b470d4349edc950db366569c519
zeev mindali <mindalizeev@gmail.com>
2022-08-29 10:29:59 +0300

    dudi

---
+++ ServerSide/basic_node_js/snir.js
@@ -0,0 +1,4 @@
+module.exports = {
+    sayHello: (name) => "Hello " + name,
+    sayBye: (name) => `${name} love to see you walk , hate to see you leave`,
+};
--- ServerSide/basic_node_js/targil1.js
+++ ServerSide/basic_node_js/targil1.js
@@ -1,4 +1,5 @@
 let mySolution = require("./targil1Functions");
+let snirFunction = require("./snir");
 
 mySolution.replace(2, 8);
 mySolution.daniel(22, 88);
