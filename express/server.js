70e8e20f92c1a8fc2aa16e16c34ca072f7f4ab22
zeev mindali <mindalizeev@gmail.com>
2022-09-05 10:15:04 +0300

    express init

---
+++ express/server.js
@@ -0,0 +1,22 @@
+//create a server
+const { request } = require('express');
+const express = require('express');
+const app = express();
+
+//on which port we will listen
+app.listen(3000);
+
+//set our engine
+app.set('view engine','ejs');
+
+app.get("/", (req,res)=>{
+    //res.send("Hello class 82");
+    console.log("root data was sent");
+    //res.sendStatus(500);
+    //res.status(500).send("איך אני מרענן את הדף?");
+    //res.status(500).json({err:"500", message:"oops i did it again",owner:"matan"});
+    //res.download('server.js');
+
+    res.render('index');
+
+});
