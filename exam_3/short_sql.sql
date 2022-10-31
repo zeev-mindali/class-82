c7fa9f6df8012d4b867446833bbbbfee3d1a2866
zeev mindali <mindalizeev@gmail.com>
2022-10-31 12:21:05 +0200

    dudi

---
+++ exam_3/short_sql.sql
@@ -0,0 +1,30 @@
+
+DROP TABLE IF EXISTS `manufactors`;
+
+CREATE TABLE `manufactors` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `manufactor_name` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name_UNIQUE` (`manufactor_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+LOCK TABLES `manufactors` WRITE;
+INSERT INTO `manufactors` VALUES (2,'apple'),(3,'ibm'),(1,'microsoft');
+UNLOCK TABLES;
+
+DROP TABLE IF EXISTS `products`;
+CREATE TABLE `products` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `product_name` varchar(45) NOT NULL,
+  `price` decimal(10,0) DEFAULT NULL,
+  `manufactor_id` int DEFAULT NULL,
+  `units` int DEFAULT NULL,
+  `last_update` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `pord_vs_manuf_idx` (`manufactor_id`),
+  CONSTRAINT `prod_vs_manuf` FOREIGN KEY (`manufactor_id`) REFERENCES `manufactors` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+LOCK TABLES `products` WRITE;
+INSERT INTO `products` VALUES (1,'iphone 14 pro max 256',4990,2,100,'2022-10-24 10:27:15');
+UNLOCK TABLES;
