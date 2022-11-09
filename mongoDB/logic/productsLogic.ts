b3c43caf25bf5baa3f6f6a4472bd3eb141b7e934
zeev mindali <mindalizeev@gmail.com>
2022-11-09 15:21:56 +0200

    mongoDB final

--- mongoDB/logic/productsLogic.ts
+++ mongoDB/logic/productsLogic.ts
@@ -49,6 +49,45 @@ const getPartialProducts= ():Promise<IProductModel[]>=>{
 }
 
 
+/*
+    SQL     : SELECT * FROM products WHERE price = 10 
+    MongoDB : return ProductModel.find({price:10}).exec();
+
+    SQL     : SELECT * FROM products WHERE price = 10 AND name = 'Eden'
+    MongoDB : return ProductModel.find({price:10, name: 'Eden'}).exec();
+
+    SQL     : SELECT * FROM products WHERE price = 10 OR name = 'Dolev'
+    MongoDB : return ProductModel.find({$or : [{price:10},{name:'Dolev'}]}).exec();
+
+    MongoDB : $or, $and, $gt, $gte, $lt, $lte, $eq, $ne....
+
+    SQL     : SELECT * FROM products WHERE price BETWEEN 10 AND 20
+    MongoDB : return ProductModel.find({price: {$gte:10, $lte20}}).exec();
+
+    SQL     : SELECT name,price FROM products WHERE price BETWEEN 10 AND 20
+    MongoDB : return ProductModel.find({price: {$gte:10, $lte20}},{name:true, price:true, _id:false}).exec()
+
+    SQL     : SELECT * FROM products WHERE price BETWEEN 10 AND 20 ORDER BY price ASC name DESC
+    MongoDB : return ProductModel.find({price: {$gte:10,$lte:20}},null,{sort:{price:1, name: -1}}).exec
+
+    SQL     : SELECT * FROM products WHERE price BETWEEN 10 AND 20 ORDER BY price DESC
+    MongoDB : return ProductModel.find({price: {$gte:10,$lte:20}},null,{sort:{price: -1}}).exec
+
+    SQL     : SELECT * FROM girls LIMIT 10, skip 10, get 7  
+    MongoDB : return ProductModel.find({},null{skip:10, limit:7}).exec();   (Paganation....)
+
+    SQL     : SELECT * FROM products WHERE name LIKE '%or%'   %mindali %zeev %or%
+    MongoDB : return ProdcutModel.find({name: {$regex:/^.+or+$/}}).exec();
+              return ProdcutModel.find({name: {$regex:/^(\b\w+\b){2,}$/}}).exec();
+
+              filter !!!!!
+
+    SQL     : INNER JOIN - get all products with category for each, dont get products without category 
+    MongoDB : return ProductModel.find({categoryId: {$ne null}}).populate("category").exec()
+
+*/
+
+
 export default{
     getAllProducts,
     getOneProduct,
